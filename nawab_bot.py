a9fc17e671124711b3041dc57d3d85e35c0c4a94
Aniketh01 <anikethgireesh@gmail.com>
2018-12-15 20:51:28 +0530

    Code refactor: changed it to function views

--- nawab_bot.py
+++ nawab_bot.py
@@ -4,12 +4,19 @@ import json
 import config
 import tweepy
 
-def twitter_authenticate(config):
+def nawab_twitter_authenticate():
     auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
     auth.set_access_token(config.access_token_key, config.access_token_secret)
     api = tweepy.API(auth)
+    return api
 
-tweet = 'Hello, world!!!'
-api.update_status(status=tweet)
+def nawab_twitter_retweet(api):
+    tweet = 'Hello, world!!!'
+    api.update_status(status=tweet)
 
+def main():
+    api = nawab_twitter_authenticate()
+    nawab_twitter_retweet(api)
 
+if __name__ == "__main__":
+    main()
