2c091caad3f0ccc7e2fd6cf5f38f8d7f997cf62c
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-04-13 12:03:53 +0530

    WIP: Telegram: Initialize the telegram bot script
    This commit does:
    - Add the bot script to the retweet function

--- nawab_bot.py
+++ nawab_bot.py
@@ -12,6 +12,8 @@ import csv
 
 
 data = pd.read_csv('data.csv')
+import tg_config as tg
+import telegram
 
 
 def nawab_twitter_authenticate():
@@ -133,12 +135,17 @@ def nawab_retweet_tweet(api):
         for line in fp:
             tweet_id = int(line)
             try:
+                u = api.get_status(id=tweet_id)
+                rt_username = u.author.screen_name
                 api.retweet(tweet_id)
                 time.sleep(60)
-
+                retweet_url = 'https://twitter.com/' + rt_username +  '/status/' + str(tweet_id)
+                                
                 with open("nawab_results.log", "a") as fp:
-                    fp.write("Nawab retweeted " +
-                             str(tweet_id) + " successfully \n")
+                    fp.write("Nawab retweeted " + str(tweet_id) + " successfully \n")
+                
+                bot = telegram.Bot(token=tg.token)
+                bot.sendMessage(chat_id=tg.chat_id, text=retweet_url)
 
             except tweepy.TweepError as e:
                 with open("nawab_errors.log", "a") as fp:
