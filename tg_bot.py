515855d44da1f93c307f5f45d926097dd072e1bd
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-04-22 10:45:57 +0530

    WIP: Telegram bot: Add retweet function to the button
    This commit does
    - Add the tweepy integration to the button function for retweeting.
    
    Test case
    - It successfully retweets. Run the command /start to the bot personally and it will scrape all the tweet urls from the tid_store.txt. Be sure to wait till all tweets are scraped and don't press the button till it does as it won't retweet. Once it does, press the button and voila, it retweets.
    
    Things to improve:
    - There is redundancy in code in tg_bot.py which can be solved using partial from a python module called functools. But the problem is I don't know whether it can be used as a parameter in the callback functions.

--- tg_bot.py
+++ tg_bot.py
@@ -1,31 +1,53 @@
 import logging
 import telegram
+import config
+import tweepy
 import tg_config as tg
+import time
 
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
+from functools import partial
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+"""
+def nawab_twitter_authenticate():
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token_key, config.access_token_secret)
+    api = tweepy.API(auth)
+    return api
+"""
+
 
 def start(update, context):
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token_key, config.access_token_secret)
+    api = tweepy.API(auth)
     with open("tid_store.txt", "r") as fp:
         for line in fp:
+            u = api.get_status(id=line)
+            username = u.author.screen_name
+            url = 'https://twitter.com/' + username +  '/status/' + str(line)
             keyboard = [[InlineKeyboardButton("Retweet", callback_data=int(line))]]
             reply_markup = InlineKeyboardMarkup(keyboard)
-            update.message.reply_text('Tweet' + str(line), reply_markup = reply_markup)
-
+            update.message.reply_text(text=url, reply_markup = reply_markup)
+            #time.sleep(10)
 
 def button(update, context):
     query = update.callback_query
-
+    auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+    auth.set_access_token(config.access_token_key, config.access_token_secret)
+    api = tweepy.API(auth)
     # CallbackQueries need to be answered, even if no notification to the user is needed
     # Some clients may have trouble otherwise. See https://core.telegram.org/bots/api#callbackquery
     query.answer()
-
-    query.edit_message_text(text="Selected option: {}".format(query.data))
+    data = query.data
+    api.retweet(data)
+    query.edit_message_text(text="Retweeted: {}".format(query.data))
 
 
 def error(update, context):
