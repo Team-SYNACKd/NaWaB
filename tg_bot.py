2590f848cf33bf88afa92fce813cf50742c60769
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-05-10 22:16:49 +0530

    Telegram Bot: Integrate the existing autoretweet argument to telegram
    This commit does
    - Checks whether the retweet arg is passed, if it is passed, telegram bot won't show the retweet button and will auto retweet.
    - Default case is it won't auto retweet and will send the tweets to the telegram user

--- tg_bot.py
+++ tg_bot.py
@@ -15,11 +15,12 @@ KILL_SIGNAL = 0
 
 class Telegram_Bot(object):
 
-    def __init__(self, twitter_api, dirpath,level, isretweet, manual_tweet):
+    def __init__(self, twitter_api, dirpath, level, auto_retweet):
         self.dirpath = dirpath
         self.twitter_api = twitter_api
         self.level = level
         self.nw_logger = nawab_logger.Nawab_Logging(dirpath, level)
+        self.auto_retweet = auto_retweet
 
 
     def nawab_tg_authenticate(self):
@@ -40,20 +41,24 @@ class Telegram_Bot(object):
         job = context.job
         global KILL_SIGNAL
         tid = pd.read_csv(self.dirpath + 'tid_store.csv')
-        for index, Id in tid.iterrows():
+        for index, tid_store in tid.iterrows():
             
             try:
-                u = self.twitter_api.get_status(id=Id['Id'])
+                u = self.twitter_api.get_status(id=tid_store['Id'])
                 username = u.author.screen_name
             except tweepy.TweepError as e:
+                if self.level == logging.CRITICAL:
+                        with open(self.dirpath + "error.log", "a") as fp:
+                            fp.write('ERROR:' + time.strftime("%m/%d/%Y %I:%M:%S %p ") + "\t|Tweepy failed to get the status of the user from the " +
+                                 str(tid_store['Id']) + ' ' + ' because of ' + e.reason + "\n")
                 self.nw_logger.logger('\t|Tweepy failed to get the status of the user from the ' +
-                                        str(Id['Id']) + ' because of ' + e.reason + '\n\n', 'error', 'Error')
+                                        str(tid_store['Id']) + ' ' + ' because of ' + e.reason + '\n\n', 'error', 'Error')
                 pass
             url = 'https://twitter.com/' + \
-                username + '/status/' + str(Id['Id'])
+                username + '/status/' + str(tid_store['Id'])
                 
-            if job.context in config.tg_admin_id:
-                keyboard = [[InlineKeyboardButton("Retweet", callback_data=int(Id['Id'])),InlineKeyboardButton("View", url=url)]]
+            if (job.context in config.tg_admin_id) and (self.auto_retweet == False or self.auto_retweet == None):
+                keyboard = [[InlineKeyboardButton("Retweet", callback_data=int(tid_store['Id'])),InlineKeyboardButton("View", url=url)]]
                 reply_markup = InlineKeyboardMarkup(keyboard)
             else:
                 keyboard = [[InlineKeyboardButton(
@@ -93,20 +98,24 @@ class Telegram_Bot(object):
             self.twitter_api.retweet(data)
         except tweepy.TweepError as e:
             
-            if self.level == 50:
+            if self.level == logging.CRITICAL:
                 with open(self.dirpath + "error.log", "a") as fp:
-                    fp.write('ERROR' + time.strftime("%m/%d/%Y %I:%M:%S %p ") + "\t|Tweepy failed to retweet after reading from the store of id " +
-                         str(data) + " because of " + e.reason + "\n")
+                    fp.write('ERROR:' + time.strftime("%m/%d/%Y %I:%M:%S %p ") + "\t|Tweepy failed to retweet after reading from the store of id " +
+                         str(data) + ' ' +  ' because of ' + e.reason + "\n")
                 
             self.nw_logger.logger('\t|Tweepy failed to retweet after reading from the store of id ' +
-                                str(data) + ' because of ' + e.reason + '\n\n', 'error', 'Error')
+                                str(data) + ' ' + ' because of ' + e.reason + '\n\n', 'error', 'Error')
             pass
         try:
             u = self.twitter_api.get_status(id=int(data))
             username = u.author.screen_name
         except tweepy.TweepError as e:
+            if self.level == logging.CRITICAL:
+                with open(self.dirpath + "error.log", "a") as fp:
+                    fp.write('ERROR:' + time.strftime("%m/%d/%Y %I:%M:%S %p ") + "\t|Tweepy failed to get the status of the user from the " +
+                                    str(data) + ' ' + ' because of ' + e.reason + '\n\n')
             self.nw_logger.logger('\t|Tweepy failed to get the status of the user from the ' +
-                                    str(data) + ' because of ' + e.reason + '\n\n', 'error', 'Error')
+                                    str(data) + ' ' + ' because of ' + e.reason + '\n\n', 'error', 'Error')
             pass
         url = 'https://twitter.com/' + \
             username + '/status/' + str(data)
@@ -118,9 +127,9 @@ class Telegram_Bot(object):
 
     def error(self, update, context):
         """Log Errors caused by Updates."""
-        if self.level == 50:
+        if self.level == logging.CRITICAL:
             with open(self.dirpath + "error.log", "a") as fp:
-                fp.write('ERROR' + time.strftime("%m/%d/%Y %I:%M:%S %p ") + '\t|Update' + update + 'caused error ' + context.error + '\n\n')
+                fp.write('ERROR:' + time.strftime("%m/%d/%Y %I:%M:%S %p ") + '\t|Update' + update + 'caused error ' + context.error + '\n\n')
                 
         self.nw_logger.logger(
             '\t|Update' + update + 'caused error ' + context.error + '\n\n', 'error', 'Error')
