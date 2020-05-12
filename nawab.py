a7eab90914506d005c351df82207a5e71f2aeb1c
Akshay Praveen Nair <akshaypraveennair11@gmail.com>
2020-05-12 13:14:11 +0530

    Nawab: Introduce Class to handle methods
    This commit does
    - wraps the functions in a class

--- nawab.py
+++ nawab.py
@@ -9,36 +9,42 @@ import threading
 from telegram.ext import Updater, CommandHandler, CallbackQueryHandler, ConversationHandler
 
 
-def retrieve_twitter_auth(dirpath, data):
-    tw_bot = twitter_bot.Twitter_Bot(dirpath, data)
-    api = tw_bot.nawab_twitter_authenticate()
-    return tw_bot, api
+class Nawab(object):
+
+    def __init__(self, dirpath, data):
+        self.dirpath = dirpath
+        self.data = data
+
+    def retrieve_twitter_auth(self):
+        tw_bot = twitter_bot.Twitter_Bot(self.dirpath, self.data)
+        api = tw_bot.nawab_twitter_authenticate()
+        return tw_bot, api
 
 
-def twitter_bot_run(data, dirpath):
-    tw_bot, api = retrieve_twitter_auth(dirpath, data)
-    tw_bot.nawab_curate_list(api)
-    tw_bot.nawab_retweet_tweet(api)
+    def twitter_bot_run(self):
+        tw_bot, api = self.retrieve_twitter_auth()
+        tw_bot.nawab_curate_list(api)
+        tw_bot.nawab_retweet_tweet(api)
 
 
-def tg_bot_run(data, dirpath):
-    tw_bot, api = retrieve_twitter_auth(dirpath, data)
-    bot = tg_bot.Telegram_Bot(api, dirpath)
-    updater = bot.nawab_tg_authenticate()
+    def tg_bot_run(self):
+        tw_bot, api = self.retrieve_twitter_auth()
+        bot = tg_bot.Telegram_Bot(api, self.dirpath)
+        updater = bot.nawab_tg_authenticate()
 
-    dp = updater.dispatcher
-    dp.add_handler(CommandHandler('start', bot.start))
-    dp.add_handler(CommandHandler("stop", bot.stop, pass_chat_data=True))
-    dp.add_handler(CommandHandler('help',bot.help))
-    dp.add_handler(CallbackQueryHandler(bot.button))
-    dp.add_error_handler(bot.error)
+        dp = updater.dispatcher
+        dp.add_handler(CommandHandler('start', bot.start))
+        dp.add_handler(CommandHandler("stop", bot.stop, pass_chat_data=True))
+        dp.add_handler(CommandHandler('help',bot.help))
+        dp.add_handler(CallbackQueryHandler(bot.button))
+        dp.add_error_handler(bot.error)
 
-    # Start the Bot
-    updater.start_polling()
+        # Start the Bot
+        updater.start_polling()
 
-    # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
-    # SIGTERM or SIGABRT
-    updater.idle()
+        # Run the bot until the user presses Ctrl-C or the process receives SIGINT,
+        # SIGTERM or SIGABRT
+        updater.idle()
 
 
 def wrapper(func, args, res):
@@ -58,13 +64,14 @@ def main():
     ownership_command = "sudo chown %s: %s" % (u_id, default_dir)
     os.system(ownership_command)
 
+    nawab = Nawab(default_dir,data)
     #Initiate twitter bot
     thread = threading.Thread(target=wrapper, args=(
-        twitter_bot_run, (data, default_dir, ), res))
+        nawab.twitter_bot_run, (), res))
     thread.start()
     while thread.is_alive:
         #Initiate telegram bot
-        tg_bot_run(data, default_dir)
+        nawab.tg_bot_run()
 
 
 if __name__ == "__main__":
